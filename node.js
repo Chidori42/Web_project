6d6f41eb31c0e7fbc17d03726aac3452ed906cc0
chidoalx <elfagrouch9@gmail.com>
2024-06-17 09:00:44 +0100

    change the dark mode to be the def one

--- node.js
+++ node.js
@@ -1,10 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+   
+    document.body.classList.add('dark-mode');
+    
+    document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
+});
 
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
-
-document.getElementById('dark-mode-toggle').addEventListener('click', toggleDarkMode);
-
 
 
 
